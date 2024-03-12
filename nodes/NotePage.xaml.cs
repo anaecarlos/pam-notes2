49a78168055772dd8796b7a6198b4175db6c4a42
ana e tevez <anatevezvitoriahenriques@gmail.com>
2024-03-12 12:26:13 -0300

    aula3

--- nodes/NotePage.xaml.cs
+++ nodes/NotePage.xaml.cs
@@ -6,16 +6,31 @@ public partial class NotePage : ContentPage
 	public NotePage()
 	{
 		InitializeComponent();
+        if (File.Exists(_filepath))
+        {
+            TextEditor.Text = File.ReadAllText(_filepath); 
+        }
+
 	}
 
     private async void SaveButton_Clicked(object sender, EventArgs e)
     {
         File.WriteAllText(_filepath, TextEditor.Text);
-        await DisplayAlert("Sucesso", "arquivo salvo com sucesso", "ok");
+        await DisplayAlert("Sucesso", "arquivo salvo com sucesso", "Ok");
     }
 
-    private void DeleteButton_Clicked(object sender, EventArgs e)
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
     {
-
+        if(File.Exists(_filepath)) 
+        {
+            File.Delete(_filepath);
+            await DisplayAlert("Sucesso", "Arquivo deletado com sucesso!", "Ok");
+            TextEditor.Text = "";
+        }
+        else
+        {
+           await DisplayAlert("Arquivo não encontrado", "Nenhum arquivo foi encontrado", "Ok");
+        }
     }
+       
 }
